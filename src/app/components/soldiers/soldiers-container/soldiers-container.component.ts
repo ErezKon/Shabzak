bb83d13482756887b88694bac80258ab6245b673
Konforti, Erez <Erez.Konforti@Dell.com>
2024-10-30 17:03:34 +0200

    code cleanup

--- src/app/components/soldiers/soldiers-container/soldiers-container.component.ts
+++ src/app/components/soldiers/soldiers-container/soldiers-container.component.ts
@@ -4,11 +4,10 @@ import { AppState } from '../../../state-management/states/app.state';
 
 import * as soldierActions from '../../../state-management/actions/soldiers.actions';
 import { Soldier } from '../../../models/soldier.model';
-import { BehaviorSubject, Observable, map, of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { selectSoldiers } from '../../../state-management/selectors/soldiers.selector';
 import { SoldiersComponent } from '../soldiers/soldiers.component';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
 import { SoldiersAddEditComponent } from '../soldiers-add-edit/soldiers-add-edit.component';
 import { SoldiersFilterComponent } from '../soldiers-filter/soldiers-filter.component';
@@ -16,8 +15,6 @@ import { SoldiersFilter } from '../soldiers-filter/soldiers-filter.model';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { BaseComponent } from '../../../utils/base-component/base-component.component';
-import { UserService } from '../../../services/user.service';
-import { log } from 'console';
 
 @Component({
   selector: 'app-soldiers-container',
@@ -32,7 +29,7 @@ export class SoldiersContainerComponent extends BaseComponent{
   soldiers$ = new BehaviorSubject<Array<Soldier>>([]);
   allSoldiers: Array<Soldier> = [];
 
-  constructor(private store: Store<AppState>, public dialog: MatDialog, private userService: UserService) {
+  constructor(private store: Store<AppState>, public dialog: MatDialog) {
     super();
     this.store.dispatch(soldierActions.getSoldiers());
     this.addSub(this.store.pipe(select(selectSoldiers))
@@ -90,12 +87,5 @@ export class SoldiersContainerComponent extends BaseComponent{
 
   onDeleteSoldier(soldierId: number) {
     this.store.dispatch(soldierActions.deleteSoldier({soldierId: soldierId}));
-  }
-
-  createUser() {
-    this.userService.login('asd','jjhsad7HSa')
-    .subscribe(val => {
-      console.log(val);
-    });
   }
 }
