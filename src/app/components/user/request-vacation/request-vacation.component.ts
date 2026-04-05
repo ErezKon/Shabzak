f86081952808af5e4ad9b64d39e4e8cceecd4777
Erez Konforti <Erez.Konforti@dell.com>
2026-04-05 18:57:58 +0300

    personal page and vacations

--- src/app/components/user/request-vacation/request-vacation.component.ts
+++ src/app/components/user/request-vacation/request-vacation.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state-management/states/app.state';
+
+import * as soldierActions from '../../../state-management/actions/soldiers.actions';
 
 @Component({
   selector: 'app-request-vacation',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    ReactiveFormsModule
+  ],
+  providers: [provideNativeDateAdapter(), {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}],
   templateUrl: './request-vacation.component.html',
   styleUrl: './request-vacation.component.scss'
 })
 export class RequestVacationComponent {
+  @Input() soldierId?: number;
 
+  startDateForm = new FormControl<Date | null>(null, [Validators.required]);
+  endDateForm = new FormControl<Date | null>(null, [Validators.required]);
+
+  constructor(private store: Store<AppState>) {}
+
+  onSendRequest() {
+    if (this.startDateForm.valid && this.endDateForm.valid && this.soldierId) {
+      this.store.dispatch(soldierActions.requestVacation({
+        soldierId: this.soldierId,
+        from: this.startDateForm.value!,
+        to: this.endDateForm.value!
+      }));
+      this.startDateForm.reset();
+      this.endDateForm.reset();
+    }
+  }
 }
--- src/app/components/user/show-vacations/show-vacations.component.ts
+++ src/app/components/user/show-vacations/show-vacations.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../../state-management/states/app.state';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { BaseComponent } from '../../../utils/base-component/base-component.component';
+import { Vacation } from '../../../models/vacation.model';
+import { VacationRequestStatus } from '../../../models/vacation-request-stats.enum';
+import { selectVacations } from '../../../state-management/selectors/soldiers.selector';
+import { CommonModule } from '@angular/common';
+
+import * as soldierActions from '../../../state-management/actions/soldiers.actions';
 
 @Component({
   selector: 'app-show-vacations',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    BaseComponent
+  ],
   templateUrl: './show-vacations.component.html',
   styleUrl: './show-vacations.component.scss'
 })
-export class ShowVacationsComponent {
+export class ShowVacationsComponent extends BaseComponent implements OnInit {
+  @Input() soldierId?: number;
 
+  displayedColumns: string[] = ['from', 'to', 'approved'];
+  dataSource = new MatTableDataSource<Vacation>();
+
+  constructor(private store: Store<AppState>) {
+    super();
+  }
+
+  ngOnInit(): void {
+    if (this.soldierId) {
+      this.store.dispatch(soldierActions.getVacations({ soldierId: this.soldierId }));
+    }
+    this.addSub(this.store.pipe(select(selectVacations)).subscribe(vacations => {
+      this.dataSource = new MatTableDataSource(vacations);
+    }));
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString('en-GB');
+  }
+
+  translateStatus(status: VacationRequestStatus): string {
+    switch (status) {
+      case VacationRequestStatus.Pending: return 'ממתין';
+      case VacationRequestStatus.Approved: return 'מאושר';
+      case VacationRequestStatus.Denied: return 'נדחה';
+      default: return '';
+    }
+  }
 }
