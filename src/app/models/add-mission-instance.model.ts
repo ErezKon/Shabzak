528a365d8eb903ec8427fe0c6a1ca13dfcb392a0
Erez Konforti <Erez.Konforti@dell.com>
2026-04-05 18:59:55 +0300

    add enddate to AddMissionInstance

--- src/app/models/add-mission-instance.model.ts
+++ src/app/models/add-mission-instance.model.ts
@@ -3,6 +3,7 @@ import { SoldierMission } from "./soldier-mission.model";
 export interface AddMissionInstance {
     id: number;
     date: string;
+    endDate?: string;
     from: string;
     to: string;
     soldiers?: Array<SoldierMission>;
